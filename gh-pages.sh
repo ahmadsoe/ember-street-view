d192aae9f372b5b40a1f79c18e345a92d883f66e
ahmadsoe <ahmadsoe@gmail.com>
2015-03-15 00:39:38 +0700

    update gh-pages script

--- gh-pages.sh
+++ gh-pages.sh
@@ -3,8 +3,8 @@ git branch -D gh-pages
 git push origin --delete gh-pages
 git checkout -b gh-pages
 ember build --environment production
-git rm -rf app addon config lib public tests
-git rm -rf Brocfile.js bower.json package.json testem.json
+git rm -rf app addon config vendor tests
+git rm -rf Brocfile.js index.js bower.json package.json testem.json
 git rm -rf .bowerrc .editorconfig .jshintrc .travis.yml
 mv dist/* .
 rm -rf dist
